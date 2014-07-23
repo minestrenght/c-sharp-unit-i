778a979e3bdf1232d663a6d09440074ad43af45b
Your Name <you@example.com>
2014-07-23 15:39:06 +0530

    added demo of type of constructors

---
+++ ConstructorTypes/ConstructorTypes/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ConstructorTypes
+{
+	class MyClass
+	{
+		static MyClass ()
+		{
+			Console.WriteLine ("Static constructor");
+		}
+
+		public MyClass ()
+		{
+			Console.WriteLine ("Default constructor");
+		}
+
+		public MyClass (char c)
+		{
+			Console.WriteLine ("Parameter constructor");
+		}
+	}
+
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			new MyClass ('A');
+			for (byte i = 1; 3 >= i; i++) {
+				new MyClass ();
+			}
+			Console.Write ("Press any key to exit ...");
+			Console.ReadKey ();
+		}
+	}
+}
