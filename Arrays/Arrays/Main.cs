fd6b995ab1ef3abfd9e60637a96b9c9ce9a7f77f
minestrenght <minestrenght@gmail.com>
2014-07-23 08:07:45 +0530

    added few demo

---
+++ Arrays/Arrays/Main.cs
@@ -0,0 +1,41 @@
+using System;
+
+namespace Arrays
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			//single dimension array
+			char[] letters = new char[2];//no default values
+			letters[0] = 'd';
+			letters[1] = '9';
+			Console.WriteLine ("Array values: {0}, {1}", letters[0], letters[1]);
+
+			bool[] flags = {true, false, true};//default values
+			Console.Write("Flag values: ");
+			foreach(bool b in flags) {
+				Console.Write("{0}, ", b);
+			}
+			Console.WriteLine();
+
+			//multi-dimensional array
+			byte[,] by = new byte[2, 3];//no values
+			Console.WriteLine("Total elements in array: {0}", by.Length);
+			Console.WriteLine("Size in 1st dim: {0}", by.GetLength(0));
+			by[0, 1] = 8;//assign values
+			Console.WriteLine("Elements in 1st dim: {0}, {1}", by[0,0], by[0,1]);
+			Console.WriteLine();
+
+			char[,] vowels = {
+				{'a', 'e'},
+				{'i', 'o'},
+				{'u'}
+			};
+			Console.Write("Vowels are: ");
+			foreach(char c in vowels) {
+				Console.Write("{0}, ", c);
+			}
+		}
+	}
+}
---
+++ DataTypes/DataTypes/Main.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace DataTypes
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			byte b = 12;
+			bool t = true;
+			int n = 23;
+			short s = 33;
+			long l = 78;
+			char c = 'A';
+			string str = "hi there";
+			float f = 3.14f;
+			double d = 09.991;
+			Console.WriteLine("{0}, {1}, {2}, {3}, {4}, {5}, {6}, {7}, {8}",b,t,n,s,l,c,str,f,d);
+		}
+	}
+}
